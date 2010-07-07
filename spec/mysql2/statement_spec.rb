390a542bb7daadc459fb52d7cad4a1880ee2bd6f
Aaron Patterson <aaron.patterson@gmail.com>
2010-07-07 09:40:18 -0700

    we can prepare statements!

---
+++ spec/mysql2/statement_spec.rb
@@ -0,0 +1,29 @@
+# encoding: UTF-8
+require 'spec_helper'
+
+describe Mysql2::Statement do
+  before :all do
+    @client = Mysql2::Client.new :host => "localhost", :username => "root"
+  end
+
+  it "should create a statement" do
+    stmt = @client.create_statement
+    stmt.should be_kind_of Mysql2::Statement
+  end
+
+  it "prepares some sql" do
+    stmt = @client.create_statement
+    lambda { stmt.prepare 'SELECT 1' }.should_not raise_error
+  end
+
+  it "return self when prepare some sql" do
+    stmt = @client.create_statement
+    stmt.prepare('SELECT 1').should == stmt
+  end
+
+  it "should raise an exception when server disconnects" do
+    stmt = @client.create_statement
+    @client.close
+    lambda { stmt.prepare 'SELECT 1' }.should raise_error(Mysql2::Error)
+  end
+end

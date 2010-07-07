a5d8a087a7ecfedeb34a5ef1ca675a710091700b
Aaron Patterson <aaron.patterson@gmail.com>
2010-07-07 09:47:41 -0700

    statements can count parameters

--- ext/mysql2/statement.c
+++ ext/mysql2/statement.c
@@ -9,7 +9,6 @@ VALUE cMysql2Statement;
 static VALUE prepare(VALUE self, VALUE sql)
 {
   MYSQL_STMT * stmt;
-
   Data_Get_Struct(self, MYSQL_STMT, stmt);
 
   if(mysql_stmt_prepare(stmt, StringValuePtr(sql), RSTRING_LEN(sql))) {
@@ -19,9 +18,22 @@ static VALUE prepare(VALUE self, VALUE sql)
   return self;
 }
 
+/* call-seq: stmt.param_count # => 2
+ *
+ * Returns the number of parameters the prepared statement expects.
+ */
+static VALUE param_count(VALUE self)
+{
+  MYSQL_STMT * stmt;
+  Data_Get_Struct(self, MYSQL_STMT, stmt);
+
+  return ULL2NUM(mysql_stmt_param_count(stmt));
+}
+
 void init_mysql2_statement()
 {
   cMysql2Statement = rb_define_class_under(mMysql2, "Statement", rb_cObject);
 
   rb_define_method(cMysql2Statement, "prepare", prepare, 1);
+  rb_define_method(cMysql2Statement, "param_count", param_count, 0);
 }

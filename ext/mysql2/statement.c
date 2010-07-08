2f44199182d8fa2bb5ddb32d870abf75767d1dc3
Aaron Patterson <aaron.patterson@gmail.com>
2010-07-08 15:55:16 -0700

    prepared statements will tell us the field count

--- ext/mysql2/statement.c
+++ ext/mysql2/statement.c
@@ -15,9 +15,22 @@ static VALUE param_count(VALUE self)
   return ULL2NUM(mysql_stmt_param_count(stmt));
 }
 
+/* call-seq: stmt.field_count # => Numeric
+ *
+ * Returns the number of fields the prepared statement returns.
+ */
+static VALUE field_count(VALUE self)
+{
+  MYSQL_STMT * stmt;
+  Data_Get_Struct(self, MYSQL_STMT, stmt);
+
+  return UINT2NUM(mysql_stmt_field_count(stmt));
+}
+
 void init_mysql2_statement()
 {
   cMysql2Statement = rb_define_class_under(mMysql2, "Statement", rb_cObject);
 
   rb_define_method(cMysql2Statement, "param_count", param_count, 0);
+  rb_define_method(cMysql2Statement, "field_count", field_count, 0);
 }

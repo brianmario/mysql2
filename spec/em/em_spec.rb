687487d5a5cc675d9a91f2998d4470a633052a65
Anton Mironov <ant.mironov@gmail.com>
2010-09-27 21:46:30 +0800

    Detach before executing callbacks.
    This allows to make queries in callbacks.

--- spec/em/em_spec.rb
+++ spec/em/em_spec.rb
@@ -19,8 +19,27 @@ describe Mysql2::EM::Client do
         results << result.first
       end
     end
-    
+
     results[0].keys.should include("second_query")
     results[1].keys.should include("first_query")
+  end
+
+  it "should support queries in callbacks" do
+    results = []
+    EM.run do
+      client = Mysql2::EM::Client.new
+      defer1 = client.query "SELECT sleep(0.025) as first_query"
+      defer1.callback do |result|
+        results << result.first
+        defer2 = client.query "SELECT sleep(0.025) as second_query"
+        defer2.callback do |result|
+          results << result.first
+          EM.stop_event_loop
+        end
+      end
+    end
+
+    results[0].keys.should include("first_query")
+    results[1].keys.should include("second_query")
   end
 end

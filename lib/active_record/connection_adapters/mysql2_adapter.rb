96aa5d2fe579737aa3ea122a037867486a1c7b9e
Brian Lopez <seniorlopez@gmail.com>
2010-10-19 08:21:41 -0700

    make sure we always set wait_timeout even if a bad value was given

--- lib/active_record/connection_adapters/mysql2_adapter.rb
+++ lib/active_record/connection_adapters/mysql2_adapter.rb
@@ -622,8 +622,9 @@ module ActiveRecord
           variable_assignments << "NAMES '#{encoding}'" if encoding
 
           # increase timeout so mysql server doesn't disconnect us
-          wait_timeout = @config[:wait_timeout] || 2592000
-          variable_assignments << "@@wait_timeout = #{wait_timeout}" if wait_timeout.is_a?(Fixnum)
+          wait_timeout = @config[:wait_timeout]
+          wait_timeout = 2592000 unless wait_timeout.is_a?(Fixnum)
+          variable_assignments << "@@wait_timeout = #{wait_timeout}"
 
           execute("SET #{variable_assignments.join(', ')}", :skip_logging)
         end

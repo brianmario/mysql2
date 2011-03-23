f5fbaf3895b47f53c7ee05fc6f1d2405b52542e8
Brian Lopez <seniorlopez@gmail.com>
2011-03-23 14:22:00 -0700

    fix error spec to take Encoding.default_internal into account

--- spec/mysql2/error_spec.rb
+++ spec/mysql2/error_spec.rb
@@ -9,6 +9,13 @@ describe Mysql2::Error do
     rescue Mysql2::Error => e
       @error = e
     end
+
+    @client2 = Mysql2::Client.new :encoding => "big5"
+    begin
+      @client2.query("HAHAHA")
+    rescue Mysql2::Error => e
+      @error2 = e
+    end
   end
 
   it "should respond to #error_number" do
@@ -29,16 +36,34 @@ describe Mysql2::Error do
   end
 
   if RUBY_VERSION =~ /1.9/
-    it "#message encoding should match the connection's encoding" do
-      @error.message.encoding.should eql(@client.encoding)
+    it "#message encoding should match the connection's encoding, or Encoding.default_internal if set" do
+      if Encoding.default_internal.nil?
+        @error.message.encoding.should eql(@client.encoding)
+        @error2.message.encoding.should eql(@client2.encoding)
+      else
+        @error.message.encoding.should eql(Encoding.default_internal)
+        @error2.message.encoding.should eql(Encoding.default_internal)
+      end
     end
 
-    it "#error encoding should match the connection's encoding" do
-      @error.error.encoding.should eql(@client.encoding)
+    it "#error encoding should match the connection's encoding, or Encoding.default_internal if set" do
+      if Encoding.default_internal.nil?
+        @error.error.encoding.should eql(@client.encoding)
+        @error2.error.encoding.should eql(@client2.encoding)
+      else
+        @error.error.encoding.should eql(Encoding.default_internal)
+        @error2.error.encoding.should eql(Encoding.default_internal)
+      end
     end
 
-    it "#sql_state encoding should match the connection's encoding" do
-      @error.sql_state.encoding.should eql(@client.encoding)
+    it "#sql_state encoding should match the connection's encoding, or Encoding.default_internal if set" do
+      if Encoding.default_internal.nil?
+        @error.sql_state.encoding.should eql(@client.encoding)
+        @error2.sql_state.encoding.should eql(@client2.encoding)
+      else
+        @error.sql_state.encoding.should eql(Encoding.default_internal)
+        @error2.sql_state.encoding.should eql(Encoding.default_internal)
+      end
     end
   end
 end

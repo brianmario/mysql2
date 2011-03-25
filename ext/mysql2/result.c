9f9e16d5a91943bb3879904d438c0f4ed9137904
Eric Wong <normalperson@yhbt.net>
2011-03-24 17:22:12 -0700

    result: mark globals as static to limit visibility

--- ext/mysql2/result.c
+++ ext/mysql2/result.c
@@ -1,7 +1,7 @@
 #include <mysql2_ext.h>
 
 #ifdef HAVE_RUBY_ENCODING_H
-rb_encoding *binaryEncoding;
+static rb_encoding *binaryEncoding;
 #endif
 
 #define MYSQL2_MAX_YEAR 2058
@@ -14,9 +14,9 @@ rb_encoding *binaryEncoding;
 #define MYSQL2_MIN_YEAR 1970
 #endif
 
-VALUE cMysql2Result;
-VALUE cBigDecimal, cDate, cDateTime;
-VALUE opt_decimal_zero, opt_float_zero, opt_time_year, opt_time_month, opt_utc_offset;
+static VALUE cMysql2Result;
+static VALUE cBigDecimal, cDate, cDateTime;
+static VALUE opt_decimal_zero, opt_float_zero, opt_time_year, opt_time_month, opt_utc_offset;
 extern VALUE mMysql2, cMysql2Client, cMysql2Error;
 static VALUE intern_encoding_from_charset;
 static ID intern_new, intern_utc, intern_local, intern_encoding_from_charset_code,

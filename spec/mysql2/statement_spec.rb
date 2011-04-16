5655058e2b34ec7b101f0c9632c5ed5164f453f1
Brian Lopez <seniorlopez@gmail.com>
2011-04-15 21:25:01 -0700

    simplify prepared statement creation into Mysql2::Client#prepare

--- spec/mysql2/statement_spec.rb
+++ spec/mysql2/statement_spec.rb
@@ -7,85 +7,63 @@ describe Mysql2::Statement do
   end
 
   it "should create a statement" do
-    stmt = @client.create_statement
-    stmt.should be_kind_of Mysql2::Statement
-  end
-
-  it "prepares some sql" do
-    stmt = @client.create_statement
-    lambda { stmt.prepare 'SELECT 1' }.should_not raise_error
-  end
-
-  it "return self when prepare some sql" do
-    stmt = @client.create_statement
-    stmt.prepare('SELECT 1').should == stmt
+    statement = nil
+    lambda { statement = @client.prepare 'SELECT 1' }.should_not raise_error
+    statement.should be_kind_of Mysql2::Statement
   end
 
   it "should raise an exception when server disconnects" do
-    stmt = @client.create_statement
     @client.close
-    lambda { stmt.prepare 'SELECT 1' }.should raise_error(Mysql2::Error)
+    lambda { @client.prepare 'SELECT 1' }.should raise_error(Mysql2::Error)
   end
 
   it "should tell us the param count" do
-    stmt = @client.create_statement
-    stmt.prepare 'SELECT ?, ?'
-    stmt.param_count.should == 2
+    statement = @client.prepare 'SELECT ?, ?'
+    statement.param_count.should == 2
 
-    stmt.prepare 'SELECT 1'
-    stmt.param_count.should == 0
+    statement2 = @client.prepare 'SELECT 1'
+    statement2.param_count.should == 0
   end
 
   it "should tell us the field count" do
-    stmt = @client.create_statement
-    stmt.prepare 'SELECT ?, ?'
-    stmt.field_count.should == 2
+    statement = @client.prepare 'SELECT ?, ?'
+    statement.field_count.should == 2
 
-    stmt.prepare 'SELECT 1'
-    stmt.field_count.should == 1
+    statement2 = @client.prepare 'SELECT 1'
+    statement2.field_count.should == 1
   end
 
   it "should let us execute our statement" do
-    stmt = @client.create_statement
-    stmt.prepare 'SELECT 1'
-    stmt.execute.should == stmt
-  end
-
-  it "should raise an exception on error" do
-    stmt = @client.create_statement
-    lambda { stmt.execute }.should raise_error(Mysql2::Error)
+    statement = @client.prepare 'SELECT 1'
+    statement.execute.should == statement
   end
 
   it "should raise an exception without a block" do
-    stmt = @client.create_statement
-    stmt.prepare 'SELECT 1'
-    stmt.execute
-    lambda { stmt.each }.should raise_error
+    statement = @client.prepare 'SELECT 1'
+    statement.execute
+    lambda { statement.each }.should raise_error
   end
 
   it "should let us iterate over results" do
-    stmt = @client.create_statement
-    stmt.prepare 'SELECT 1'
-    stmt.execute
+    statement = @client.prepare 'SELECT 1'
+    statement.execute
     rows = []
-    stmt.each { |row| rows << row }
+    statement.each { |row| rows << row }
     rows.should == [[1]]
   end
 
   it "should select dates" do
-    stmt = @client.create_statement
-    stmt.prepare 'SELECT NOW()'
-    stmt.execute
+    statement = @client.prepare 'SELECT NOW()'
+    statement.execute
     rows = []
-    stmt.each { |row| rows << row }
+    statement.each { |row| rows << row }
     rows.first.first.should be_kind_of Time
   end
 
   it "should tell us about the fields" do
-    stmt = @client.create_statement
-    stmt.prepare 'SELECT 1 as foo, 2'
-    stmt.execute
-    list = stmt.fields
+    statement = @client.prepare 'SELECT 1 as foo, 2'
+    statement.execute
+    list = statement.fields
     list.length.should == 2
     list.first.name.should == 'foo'
     list[1].name.should == '2'

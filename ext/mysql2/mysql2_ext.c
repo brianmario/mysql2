2648b0f0fbaa423950144c90eeb5e1fb87a9101c
Brian Lopez <seniorlopez@gmail.com>
2011-05-05 12:58:57 -0700

    remove old create_statement function

--- ext/mysql2/mysql2_ext.c
+++ ext/mysql2/mysql2_ext.c
@@ -2,21 +2,6 @@
 
 VALUE mMysql2, cMysql2Error;
 
-/* call-seq: client.create_statement # => Mysql2::Statement
- *
- * Create a new prepared statement.
- */
-static VALUE create_statement(VALUE self)
-{
-  MYSQL * client;
-  MYSQL_STMT * stmt;
-
-  Data_Get_Struct(self, MYSQL, client);
-  stmt = mysql_stmt_init(client);
-
-  return Data_Wrap_Struct(cMysql2Statement, 0, mysql_stmt_close, stmt);
-}
-
 /* Ruby Extension initializer */
 void Init_mysql2() {
   mMysql2      = rb_define_module("Mysql2");

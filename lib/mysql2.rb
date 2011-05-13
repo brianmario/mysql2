49793188d63fb660722a66135485a4b1eb1497b8
Brian Lopez <seniorlopez@gmail.com>
2011-05-12 18:34:55 -0700

    add warning about using 0.2.x in Rails 3.1

--- lib/mysql2.rb
+++ lib/mysql2.rb
@@ -14,3 +14,8 @@ require 'mysql2/result'
 # A modern, simple and very fast Mysql library for Ruby - binding to libmysql
 module Mysql2
 end
+
+if defined?(ActiveRecord::VERSION::STRING) && ActiveRecord::VERSION::STRING >= "3.1"
+  puts "WARNING: This version of mysql2 (#{Mysql2::VERSION}) isn't compatible with Rails 3.1 as the ActiveRecord adapter was pulled into Rails itself."
+  puts "WARNING: Please use the 0.3.x (or greater) releases if you plan on using it in Rails >= 3.1.x"
+end

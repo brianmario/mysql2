4c234978c8c2cd32edf109a9e553eadc9757761d
Aaron Patterson <aaron.patterson@gmail.com>
2012-03-27 18:02:25 -0700

    guard gcc specific flags in the extconf. fixes #240

--- ext/mysql2/extconf.rb
+++ ext/mysql2/extconf.rb
@@ -62,13 +62,13 @@ end
   asplode h unless have_header h
 end
 
-unless RUBY_PLATFORM =~ /mswin/ or RUBY_PLATFORM =~ /sparc/
+# GCC specific flags
+if RbConfig::MAKEFILE_CONFIG['CC'] =~ /gcc/
   $CFLAGS << ' -Wall -funroll-loops'
-end
-# $CFLAGS << ' -O0 -ggdb3 -Wextra'
 
-if hard_mysql_path = $libs[%r{-L(/[^ ]+)}, 1]
-	$LDFLAGS << " -Wl,-rpath,#{hard_mysql_path}"
+  if hard_mysql_path = $libs[%r{-L(/[^ ]+)}, 1]
+    $LDFLAGS << " -Wl,-rpath,#{hard_mysql_path}"
+  end
 end
 
 create_makefile('mysql2/mysql2')

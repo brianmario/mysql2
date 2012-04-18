4304fe6ef37861ae82705f94db81d945f3f978d2
Iain Barnett <iainspeed@gmail.com>
2012-04-18 16:07:59 +0100

    Made the deprecation warning a bit more readable.

--- lib/mysql2/client.rb
+++ lib/mysql2/client.rb
@@ -36,7 +36,8 @@ module Mysql2
       
       if [:user,:pass,:hostname,:dbname,:db,:sock].any?{|k| @query_options.has_key?(k) }
         warn "============= WARNING FROM mysql2 ============="
-        warn "The options :user, :pass, :hostname, :dbname, :db, and :sock will be deprecated at some point in the future, please use :username, :password, :host, 'localhost', :port, :database, :socket, :flags for the options."
+        warn "The options :user, :pass, :hostname, :dbname, :db, and :sock will be deprecated at some point in the future."
+        warn "Instead, please use :username, :password, :host, 'localhost', :port, :database, :socket, :flags for the options."
         warn "============= END WARNING FROM mysql2 ========="
       end
 

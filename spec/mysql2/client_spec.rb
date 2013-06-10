02da91ac005dcca9b15ed3e0f33bd5a407c22a40
Brian Lopez <seniorlopez@gmail.com>
2013-06-09 19:06:34 -0500

    bump timings in this test again

--- spec/mysql2/client_spec.rb
+++ spec/mysql2/client_spec.rb
@@ -260,7 +260,7 @@ describe Mysql2::Client do
           (mark[:USR1] - mark[:START]).should < 1.2
           (mark[:END] - mark[:USR1]).should > 0.9
           (mark[:END] - mark[:START]).should >= 2
-          (mark[:END] - mark[:START]).should < 2.1
+          (mark[:END] - mark[:START]).should < 2.2
           Process.kill(:TERM, pid)
           Process.waitpid2(pid)
         ensure

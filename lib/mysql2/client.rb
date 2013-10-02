1f118498452cb1cb430cbd43f3916e081aeaa177
Hiro Asari <asari.ruby@gmail.com>
2013-10-02 13:55:19 -0400

    Explicitly convert database and user names

--- lib/mysql2/client.rb
+++ lib/mysql2/client.rb
@@ -55,7 +55,7 @@ module Mysql2
       socket   = opts[:socket] || opts[:sock]
       flags    = opts[:flags] ? opts[:flags] | @query_options[:connect_flags] : @query_options[:connect_flags]
 
-      connect user, pass, host, port, database, socket, flags
+      connect user.to_s, pass, host, port, database.to_s, socket, flags
     end
 
     def self.default_query_options

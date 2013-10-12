b1e14b3be29eb51ee914be29c758f0f1c5811ee9
Aaron Stone <aaron@serendipity.cx>
2013-10-11 17:48:39 -0700

    Handle dir_config and with_config mysql more strictly, add rpath more often, add notices

--- ext/mysql2/extconf.rb
+++ ext/mysql2/extconf.rb
@@ -31,9 +31,9 @@ dirs = ENV['PATH'].split(File::PATH_SEPARATOR) + %w[
 
 GLOB = "{#{dirs.join(',')}}/{mysql_config,mysql_config5}"
 
-if RUBY_PLATFORM =~ /mswin|mingw/
-  inc, lib = dir_config('mysql')
-
+# If the user has provided a --with-mysql-dir argument, we must respect it or fail.
+inc, lib = dir_config('mysql')
+if inc && lib
   # Ruby versions not incorporating the mkmf fix at
   # https://bugs.ruby-lang.org/projects/ruby-trunk/repository/revisions/39717
   # do not properly search for lib directories, and must be corrected
@@ -41,9 +41,17 @@ if RUBY_PLATFORM =~ /mswin|mingw/
     @libdir_basename = 'lib'
     inc, lib = dir_config('mysql')
   end
-  exit 1 unless have_library("libmysql")
-elsif mc = (with_config('mysql-config') || Dir[GLOB].first) then
+  abort "-----\nCannot find include dir at #{inc}\n-----" unless inc && File.directory?(inc)
+  abort "-----\nCannot find library dir at #{lib}\n-----" unless lib && File.directory?(lib)
+  warn  "-----\nUsing --with-mysql-dir=#{File.dirname inc}\n-----"
+  rpath_dir = lib
+elsif mc = (with_config('mysql-config') || Dir[GLOB].first)
+  # If the user has provided a --with-mysql-config argument, we must respect it or fail.
+  # If the user gave --with-mysql-config with no argument means we should try to find it.
   mc = Dir[GLOB].first if mc == true
+  abort "-----\nCannot find mysql_config at #{mc}\n-----" unless mc && File.exists?(mc)
+  abort "-----\nCannot execute mysql_config at #{mc}\n-----" unless File.executable?(mc)
+  warn  "-----\nUsing mysql_config at #{mc}\n-----"
   ver = `#{mc} --version`.chomp.to_f
   includes = `#{mc} --include`.chomp
   exit 1 if $? != 0
@@ -55,6 +63,7 @@ elsif mc = (with_config('mysql-config') || Dir[GLOB].first) then
   exit 1 if $? != 0
   $INCFLAGS += ' ' + includes
   $libs = libs + " " + $libs
+  rpath_dir = libs
 else
   inc, lib = dir_config('mysql', '/usr/local')
   libs = ['m', 'z', 'socket', 'nsl', 'mygcc']
@@ -62,11 +71,16 @@ else
     exit 1 if libs.empty?
     have_library(libs.shift)
   end
+  rpath_dir = lib
 end
 
-if have_header('mysql.h') then
+if RUBY_PLATFORM =~ /mswin|mingw/
+  exit 1 unless have_library('libmysql')
+end
+
+if have_header('mysql.h')
   prefix = nil
-elsif have_header('mysql/mysql.h') then
+elsif have_header('mysql/mysql.h')
   prefix = 'mysql'
 else
   asplode 'mysql.h'
@@ -77,16 +91,20 @@ end
   asplode h unless have_header h
 end
 
-# GCC specific flags
-if RbConfig::MAKEFILE_CONFIG['CC'] =~ /gcc/
+# GCC | Clang | XCode specific flags
+if RbConfig::MAKEFILE_CONFIG['CC'] =~ /gcc|clang|xcrun/
   $CFLAGS << ' -Wall -funroll-loops'
 
-  if libdir = $libs[%r{-L(/[^ ]+)}, 1]
+  if libdir = rpath_dir[%r{(-L)?(/[^ ]+)}, 2]
     # The following comment and test is borrowed from the Pg gem:
     # Try to use runtime path linker option, even if RbConfig doesn't know about it.
     # The rpath option is usually set implicit by dir_config(), but so far not on Mac OS X.
     if RbConfig::CONFIG["RPATHFLAG"].to_s.empty? && try_link('int main() {return 0;}', " -Wl,-rpath,#{libdir}")
+      warn "-----\nSetting rpath to #{libdir}\n-----"
       $LDFLAGS << " -Wl,-rpath,#{libdir}"
+    else
+      # Make sure that LIBPATH gets set if we didn't explicitly set the rpath.
+      $LIBPATH << libdir unless $LIBPATH.include?(libdir)
     end
   end
 end

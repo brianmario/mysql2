1d4de8963c71ce88772a0e27883066160c3dbbd4
Aaron Stone <aaron@serendipity.cx>
2014-09-14 07:56:31 -0700

    Move all args to Mysql2::Error to a separate method

--- lib/mysql2/error.rb
+++ lib/mysql2/error.rb
@@ -9,18 +9,24 @@ module Mysql2
     }.freeze
 
     attr_reader :error_number, :sql_state
-    attr_writer :server_version
 
     # Mysql gem compatibility
     alias_method :errno, :error_number
     alias_method :error, :message
 
-    def initialize(msg, server_version=nil, error_number=nil, sql_state=nil)
-      self.server_version = server_version
-      self.error_number = error_number
-      self.sql_state = sql_state.respond_to?(:encode) ? sql_state.encode(ENCODE_OPTS) : sql_state
+    def initialize(msg)
+      @server_version ||= nil
 
       super(clean_message(msg))
+    end
+
+    def self.new_with_args(msg, server_version, error_number, sql_state)
+      err = allocate
+      err.instance_variable_set('@server_version', server_version)
+      err.instance_variable_set('@error_number', error_number)
+      err.instance_variable_set('@sql_state', sql_state.respond_to?(:encode) ? sql_state.encode(ENCODE_OPTS) : sql_state)
+      err.send(:initialize, msg)
+      err
     end
 
     private

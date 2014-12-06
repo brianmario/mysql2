d3c55d21835d54e91a424fae55fc14ecd7f72b57
Justin Case <hello@justincase.cx>
2014-12-07 00:27:49 +0100

    re-enable more specs

--- spec/mysql2/statement_spec.rb
+++ spec/mysql2/statement_spec.rb
@@ -6,28 +6,32 @@ describe Mysql2::Statement do
     @client = Mysql2::Client.new(DatabaseCredentials['root'].merge(:encoding => "utf8"))
   end
 
-  # it "should create a statement" do
-  #   statement = nil
-  #   lambda { statement = @client.prepare 'SELECT 1' }.should_not raise_error
-  #   statement.should be_kind_of Mysql2::Statement
-  # end
+  it "should create a statement" do
+    statement = nil
+    lambda { statement = @client.prepare 'SELECT 1' }.should_not raise_error
+    statement.should be_kind_of Mysql2::Statement
+  end
 
-  # it "should raise an exception when server disconnects" do
-  #   @client.close
-  #   lambda { @client.prepare 'SELECT 1' }.should raise_error(Mysql2::Error)
-  # end
+  it "should raise an exception when server disconnects" do
+    @client.close
+    lambda { @client.prepare 'SELECT 1' }.should raise_error(Mysql2::Error)
+  end
 
-  # it "should tell us the param count" do
-  #   statement = @client.prepare 'SELECT ?, ?'
-  #   statement.param_count.should == 2
+  it "should tell us the param count" do
+    statement = @client.prepare 'SELECT ?, ?'
+    statement.param_count.should == 2
 
-  #   statement2 = @client.prepare 'SELECT 1'
-  #   statement2.param_count.should == 0
-  # end
+    statement2 = @client.prepare 'SELECT 1'
+    statement2.param_count.should == 0
+  end
 
-  # it "should tell us the field count" do
-  #   statement = @client.prepare 'SELECT ?, ?'
-  #   statement.field_count.should == 2
+  it "should tell us the field count" do
+    statement = @client.prepare 'SELECT ?, ?'
+    statement.field_count.should == 2
+
+    statement2 = @client.prepare 'SELECT 1'
+    statement2.field_count.should == 1
+  end
 
   #   statement2 = @client.prepare 'SELECT 1'
   #   statement2.field_count.should == 1

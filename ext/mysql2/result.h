cbb469fd23f65c04f73c282382ee8acd045e56ef
nyaxt <ueno@nyaxtstep.com>
2014-12-07 01:05:09 +0100

    reuse result bind buffers

--- ext/mysql2/result.h
+++ ext/mysql2/result.h
@@ -18,6 +18,11 @@ typedef struct {
   MYSQL_RES *result;
   MYSQL_STMT *stmt;
   mysql_client_wrapper *client_wrapper;
+  /* statement result bind buffers */
+  MYSQL_BIND *result_buffers;
+  my_bool *is_null;
+  my_bool *error;
+  unsigned long *length;
 } mysql2_result_wrapper;
 
 #define GetMysql2Result(obj, sval) (sval = (mysql2_result_wrapper*)DATA_PTR(obj));

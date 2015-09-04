fe39b00288d23ca38a3978d2825e894aefc18f02
Aaron Stone <aaron@serendipity.cx>
2015-09-03 23:14:04 -0700

    Fix for "Table 'performance_schema.session_variables' doesn't exist"

--- .travis_mysql57.sh
+++ .travis_mysql57.sh
@@ -9,4 +9,6 @@ apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0x8C718D3B5072
 add-apt-repository 'deb http://repo.mysql.com/apt/ubuntu/ precise mysql-5.7-dmr'
 
 apt-get update
-apt-get -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -y install mysql-server libmysqlclient-dev
+apt-get -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew -y install mysql-server libmysqlclient-dev
+
+mysql_upgrade -u root --force --upgrade-system-tables

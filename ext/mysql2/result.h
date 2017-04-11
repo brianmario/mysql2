5f6b0ad00c31cc3140cf061eda5233901cb1f7db
Yasuo Honda <yasuo.honda@gmail.com>
2017-04-11 16:02:52 +0000

    Use `bool` instead of `my_bool` which has been removed since MySQL 8.0.1
    Refer "Changes in MySQL 8.0.1 (2017-04-10, Development Milestone)"
    https://dev.mysql.com/doc/relnotes/mysql/8.0/en/news-8-0-1.html
    
    > Incompatible Change: The mysql.h header file now requires a C++ or C99 compiler to compile.
    > The my_bool type is no longer used in MySQL source code.
    > Any third-party code that used this type to represent C boolean variables
    > should use the bool or int C type instead. (Bug #25597667)

--- ext/mysql2/result.h
+++ ext/mysql2/result.h
@@ -1,5 +1,6 @@
 #ifndef MYSQL2_RESULT_H
 #define MYSQL2_RESULT_H
+#include <stdbool.h>
 
 void init_mysql2_result(void);
 VALUE rb_mysql_result_to_obj(VALUE client, VALUE encoding, VALUE options, MYSQL_RES *r, VALUE statement);
@@ -21,8 +22,8 @@ typedef struct {
   mysql_client_wrapper *client_wrapper;
   /* statement result bind buffers */
   MYSQL_BIND *result_buffers;
-  my_bool *is_null;
-  my_bool *error;
+  bool *is_null;
+  bool *error;
   unsigned long *length;
 } mysql2_result_wrapper;
 

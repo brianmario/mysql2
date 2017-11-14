f8ee23acebad84fbbabe062d4bfc25f25e8d02c3
Aaron Stone <aaron@serendipity.cx>
2017-11-14 08:57:12 -0800

    Stash the mysql-build@oss.oracle.com key locally instead of calling pgp.mit.edu

--- .travis_mysql80.sh
+++ .travis_mysql80.sh
@@ -5,7 +5,7 @@ set -eux
 apt-get purge -qq '^mysql*' '^libmysql*'
 rm -fr /etc/mysql
 rm -fr /var/lib/mysql
-apt-key adv --keyserver pgp.mit.edu --recv-keys 5072E1F5
+apt-key add - < support/5072E1F5.asc
 add-apt-repository 'deb http://repo.mysql.com/apt/ubuntu/ trusty mysql-8.0'
 apt-get update -qq
 apt-get install -qq mysql-server libmysqlclient-dev

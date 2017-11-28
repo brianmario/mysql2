45cc5742dbbacbe81c6f34fdf15f1b88b6e9bb90
Aaron Stone <aaron@serendipity.cx>
2017-11-27 21:09:44 -0800

    Accept query options on Statement#execute

--- lib/mysql2/statement.rb
+++ lib/mysql2/statement.rb
@@ -2,9 +2,9 @@ module Mysql2
   class Statement
     include Enumerable
 
-    def execute(*args)
+    def execute(*args, **kwargs)
       Thread.handle_interrupt(::Mysql2::Util::TIMEOUT_ERROR_CLASS => :never) do
-        _execute(*args)
+        _execute(*args, **kwargs)
       end
     end
   end

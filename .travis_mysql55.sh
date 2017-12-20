b2cfca1fabe8ef698c9d0ea3db31cb2ca8485ddc
Aaron Stone <aaron@serendipity.cx>
2017-12-20 11:39:23 -0800

    Travis CI should run MySQL 5.5 tests on Trusty

---
+++ .travis_mysql55.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -eux
+
+apt-get purge -qq '^mysql*' '^libmysql*'
+rm -fr /etc/mysql
+rm -fr /var/lib/mysql
+apt-get install -qq mysql-server-5.5 mysql-client-core-5.5 mysql-client-5.5 libmysqlclient-dev
--- .travis_setup.sh
+++ .travis_setup.sh
@@ -2,6 +2,11 @@
 
 set -eux
 
+# Install MySQL 5.5 if DB=mysql55
+if [[ -n ${DB-} && x$DB =~ ^xmysql55 ]]; then
+  sudo bash .travis_mysql55.sh
+fi
+
 # Install MySQL 5.7 if DB=mysql57
 if [[ -n ${DB-} && x$DB =~ ^xmysql57 ]]; then
   sudo bash .travis_mysql57.sh

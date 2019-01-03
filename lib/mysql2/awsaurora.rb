72bf8f8bbd467b7fb4c21014fb20826eee8e6f85
nurlan <nurlan.moldomurov@percona.com>
2019-01-04 01:11:10 +0600

    Find active master using sql instead of API

--- lib/mysql2/awsaurora.rb
+++ lib/mysql2/awsaurora.rb
@@ -3,23 +3,65 @@ require 'mysql2'
 module Mysql2
   module AWSAurora
     class Client < ::Mysql2::Client
+      attr_reader :opts, :master_host_address
 
       def initialize(opts = {})
-        opts = Mysql2::Util.key_hash_as_symbols(opts)
-        @opts = opts
+        @opts = Mysql2::Util.key_hash_as_symbols(opts)
+        unless @aurora_dns_pattern
+          @aurora_dns_pattern = /(?<sub>.+)\.(?<cluster>cluster-)?(?<suffix>[a-zA-Z0-9]+\.[a-zA-Z0-9\\-]+\.rds\.amazonaws\.com)/i
+          @master_host_address = nil
+          @cluster_dns_suffix = nil
+          @blacklist_endpoints = []
+          @cluster_endpoints = []
+          find_cluster_host_address(@opts[:host])
+        end
 
-        @reconnect_attempts = opts[:reconnect_attempts] || 3
-        @initial_retry_wait = opts[:initial_retry_wait] || 0.5
-        @max_retry_wait = opts[:max_retry_wait]
-        @logger = opts[:logger]
-
-        aws_opts = Mysql2::Util.key_hash_as_symbols(@opts[:aws_opts])
-
-        @rds_client = Aws::RDS::Client.new(
-            Mysql2::Util.key_hash_as_symbols(aws_opts[:credentials])
-        )
-
+        @reconnect_attempts = @opts[:reconnect_attempts] || 3
+        @initial_retry_wait = @opts[:initial_retry_wait] || 1
+        @max_retry_wait = @opts[:max_retry_wait]
+        @logger = @opts[:logger]
         super(@opts)
+
+        update_servers_list
+        @opts[:host] = @master_host_address
+      end
+
+      def find_cluster_host_address(host)
+        if (matcher = @aurora_dns_pattern.match(host))
+          if @cluster_dns_suffix.nil?
+            @cluster_dns_suffix = matcher[:suffix]
+          else
+            if @cluster_dns_suffix.casecmp(matcher[:suffix]) != 0
+              raise "Connection string must contain only one aurora cluster. " + "'" +
+                        host + "' doesn't correspond to DNS prefix '" + @cluster_dns_suffix + "'"
+            end
+          end
+
+          if !matcher[:cluster].nil? && !matcher[:cluster].empty?
+            return host;
+          end
+        else
+          if @cluster_dns_suffix.nil? && host.contains(".")
+            parts = host.split(".", 1)
+            @cluster_dns_suffix = parts[1]
+          end
+        end
+        nil
+      end
+
+      def update_servers_list
+        results = query("select server_id, session_id from information_schema.replica_host_status " +
+                            "where last_update_timestamp > now() - INTERVAL 3 MINUTE")
+        host_addresses = []
+        results.each do |row|
+          host_addresses.push(row["server_id"] + "." + @cluster_dns_suffix)
+          if row["session_id"] == "MASTER_SESSION_ID"
+            @master_host_address = row["server_id"] + "." + @cluster_dns_suffix
+          end
+        end
+        if host_addresses
+          @cluster_endpoints = host_addresses
+        end
       end
 
       def reconnect_with_readonly(&block)
@@ -35,10 +77,23 @@ module Mysql2
                 "Reconnect with readonly: #{e.message} " \
             "(retries: #{retries}/#{@reconnect_attempts}) (wait: #{wait}sec)"
               } if @logger
-              sleep wait
               retries += 1
-              reconnect
-              @logger.debug { "Reconnect with readonly: disconnected and retry" } if @logger
+              current_host = @opts[:host]
+              begin
+                sleep wait
+                @blacklist_endpoints.push(@opts[:host])
+                endpoint = next_available_host
+                reconnect(endpoint)
+                if @master_host_address != endpoint
+                  reconnect(@master_host_address)
+                end
+              rescue Mysql2::Error::ConnectionError => e
+                warn e
+                if current_host != @opts[:host]
+                  retry
+                end
+              end
+              @logger.debug {"Reconnect with readonly: disconnected and retry"} if @logger
               retry
               # raise e
             else
@@ -52,36 +107,14 @@ module Mysql2
         end
       end
 
-      def reconnect
-        endpoint = current_writer_endpoint
-        @opts[:host] = endpoint.address
-        @opts[:port] = endpoint.port
-        close rescue nil
-        #TODO: find current writer
-        initialize(@opts)
-      end
-
-      def current_writer_endpoint
-
-        aws_opts = @opts[:aws_opts]
-
-        resp = @rds_client.describe_db_clusters(
-            {
-                db_cluster_identifier: aws_opts[:db_cluster_identifier],
-            })
-
-        clusters = resp.db_clusters[0]
-
-        writer = clusters.db_cluster_members.find(&:is_cluster_writer)
-
-        resp = @rds_client.describe_db_instances(
-            {
-                db_instance_identifier: writer.db_instance_identifier,
-            })
-
-        instance = resp.db_instances[0]
-
-        instance.endpoint
+      def next_available_host
+        @blacklist_endpoints = @blacklist_endpoints & @cluster_endpoints
+        hosts = @cluster_endpoints - @blacklist_endpoints
+        hosts = hosts + @blacklist_endpoints
+        if hosts == @blacklist_endpoints
+          @blacklist_endpoints.clear
+        end
+        hosts[0]
       end
 
       def query(sql, options = {})
@@ -90,6 +123,13 @@ module Mysql2
         end
       end
 
+      private
+
+      def reconnect(endpoint)
+        @opts[:host] = endpoint
+        close rescue nil
+        initialize(@opts)
+      end
     end
   end
 end
--- spec/awsaurora/client_spec.rb
+++ spec/awsaurora/client_spec.rb
@@ -12,22 +12,46 @@ RSpec.describe Mysql2::AWSAurora::Client do
     aws_opts = Mysql2::Util.key_hash_as_symbols(opts[:aws_opts])
 
     before(:each) do
-      puts opts
       begin
         @mysql_client = Mysql2::AWSAurora::Client.new(opts)
+      rescue Mysql2::Error => e
+        warn("cannot connect to db: " + e.message )
+      rescue Exception => e
+        warn(e.message)
+        warn(e.backtrace)
+      end
+      begin
         @rds_client = Aws::RDS::Client.new(
             Mysql2::Util.key_hash_as_symbols(aws_opts[:credentials])
         )
       rescue Exception => e
-        skip("invalid configuration: " + e.backtrace)
-      rescue Mysql2::Error => e
-        skip("cannot connect to db: " + e.message )
+        warn(e.backtrace)
       end
     end
 
-    it 'tests reconnect on failover' do
 
-      endpoint = @mysql_client.current_writer_endpoint
+    def current_writer_endpoint(db_cluster_identifier)
+      resp = @rds_client.describe_db_clusters(
+          {
+              db_cluster_identifier: db_cluster_identifier,
+          })
+
+      clusters = resp.db_clusters[0]
+
+      writer = clusters.db_cluster_members.find(&:is_cluster_writer)
+
+      resp = @rds_client.describe_db_instances(
+          {
+              db_instance_identifier: writer.db_instance_identifier,
+          })
+
+      instance = resp.db_instances[0]
+
+      instance.endpoint
+    end
+
+    it 'tests reconnect on failover' do
+      endpoint = current_writer_endpoint(aws_opts[:db_cluster_identifier])
 
       @mysql_client.query("delete from users")
 
@@ -37,20 +61,24 @@ RSpec.describe Mysql2::AWSAurora::Client do
           {
               db_cluster_identifier: aws_opts[:db_cluster_identifier],
           })
+      expect(@mysql_client.opts[:host]).to eql endpoint.address
 
+      count = 1
       # wait until cluster writer change
       while true do
-        new_endpoint = @mysql_client.current_writer_endpoint
+        @mysql_client.query("insert into users values('another text')")
+        count+=1
+        new_endpoint = current_writer_endpoint(aws_opts[:db_cluster_identifier])
         break if new_endpoint.address != endpoint.address
 
-        sleep 1
+        # puts "wait 1 sec #{new_endpoint.address} #{endpoint.address}"
+        sleep 0.2
       end
-
-      @mysql_client.query("insert into users values('another text')")
 
       results = @mysql_client.query("select * from users;")
 
-      expect(results.count).to eql(2)
+      expect(results.count).to eql(count)
+      expect(@mysql_client.opts[:host]).to eql new_endpoint.address
     end
 
   end

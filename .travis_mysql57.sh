1e2372cd94de8dd471742c2c820d9de9a0ab8f34
Felix Bünemann <buenemann@louis.info>
2019-04-26 01:41:08 +0200

    Use release version of MySQL 5.7 on Travis CI

--- .travis_mysql57.sh
+++ .travis_mysql57.sh
@@ -10,7 +10,7 @@ rm -rf /var/log/mysql
 
 apt-get install python-software-properties
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0x8C718D3B5072E1F5
-add-apt-repository 'deb http://repo.mysql.com/apt/ubuntu/ precise mysql-5.7-dmr'
+add-apt-repository 'deb http://repo.mysql.com/apt/ubuntu/ precise mysql-5.7'
 
 apt-get update
 apt-get -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold -y install mysql-server libmysqlclient-dev

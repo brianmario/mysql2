12a5e9656ef937e3f9c76a8b97a160cdc8b22f7c
Jun Aruga <jaruga@redhat.com>
2019-11-22 19:04:04 +0100

    Fix CentOS case error in Travis.
    As the `mysql_install_db` script in CentOS refers wrong path of
    resolveip command: `/usr/libexec/resolveip`,
    set `ln -s /usr/bin/resolveip /usr/libexec/resolveip` as a workflow.
    
    See https://travis-ci.org/brianmario/mysql2/jobs/615263124#L2840

--- .travis_setup_centos.sh
+++ .travis_setup_centos.sh
@@ -4,6 +4,11 @@ set -eux
 
 MYSQL_TEST_LOG="$(pwd)/mysql.log"
 
+# mysql_install_db uses wrong path for resolveip
+# https://jira.mariadb.org/browse/MDEV-18563
+# https://travis-ci.org/brianmario/mysql2/jobs/615263124#L2840
+ln -s "$(command -v resolveip)" /usr/libexec/resolveip
+
 mysql_install_db \
   --log-error="${MYSQL_TEST_LOG}"
 /usr/libexec/mysqld \

8d4e7410797023dffe0d472a133cbfb1b4186ab6
Jean Boussier <jean.boussier@gmail.com>
2021-01-25 10:01:49 +0100

    Run GC.verify_compaction_references on CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,12 @@ require 'timeout'
 require 'yaml'
 DatabaseCredentials = YAML.load_file('spec/configuration.yml')
 
+if GC.respond_to?(:verify_compaction_references)
+  # This method was added in Ruby 3.0.0. Calling it this way asks the GC to
+  # move objects around, helping to find object movement bugs.
+  GC.verify_compaction_references(double_heap: true, toward: :empty)
+end
+
 RSpec.configure do |config|
   config.disable_monkey_patching!
 

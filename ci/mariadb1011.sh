79f78f940685396f1b5f30ec502544bb7e3ba9cf
Jeremy Daer <jeremy@37signals.com>
2023-08-02 21:14:38 -0700

    CI: fix defunct MariaDB package names and repo URLs (#1326)
    10.6 repo URL was moved. 10.11 package name no longer includes version suffix.
    
    Switch both to deb.mariadb.org mirrors and use suffix-less package names.

--- ci/mariadb1011.sh
+++ ci/mariadb1011.sh
@@ -6,5 +6,5 @@ rm -fr /etc/mysql
 rm -fr /var/lib/mysql
 
 apt-key add support/C74CD1D8.asc
-add-apt-repository "deb https://downloads.mariadb.com/MariaDB/mariadb-10.11/repo/ubuntu $(lsb_release -cs) main"
-apt install -y -o Dpkg::Options::='--force-confnew' mariadb-server-10.11 libmariadb-dev
+add-apt-repository "deb https://deb.mariadb.org/10.11/ubuntu $(lsb_release -cs) main"
+apt install -y -o Dpkg::Options::='--force-confnew' mariadb-server libmariadb-dev

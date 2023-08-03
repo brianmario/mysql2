43820767bad1e7ed3bcfba950b4d5c95fef38f4d
Jeremy Daer <jeremy@37signals.com>
2023-08-02 20:48:06 -0700

    CI: fix defunct MariaDB package names and repo URLs

--- ci/mariadb106.sh
+++ ci/mariadb106.sh
@@ -6,5 +6,5 @@ rm -fr /etc/mysql
 rm -fr /var/lib/mysql
 
 apt-key add support/C74CD1D8.asc
-add-apt-repository "deb https://downloads.mariadb.com/MariaDB/mariadb-10.6/repo/ubuntu $(lsb_release -cs) main"
-apt install -y -o Dpkg::Options::='--force-confnew' mariadb-server-10.6 libmariadb-dev
+add-apt-repository "deb https://deb.mariadb.org/10.6/ubuntu $(lsb_release -cs) main"
+apt install -y -o Dpkg::Options::='--force-confnew' mariadb-server libmariadb-dev

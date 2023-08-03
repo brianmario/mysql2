e58a43e297b0c3ab533a959a547caf5a0a3b6d69
Aaron Stone <aaron@serendipity.cx>
2023-08-02 18:26:25 -0700

    CI: Update MariaDB 10.11 package name

--- ci/mariadb1011.sh
+++ ci/mariadb1011.sh
@@ -7,4 +7,4 @@ rm -fr /var/lib/mysql
 
 apt-key add support/C74CD1D8.asc
 add-apt-repository "deb https://downloads.mariadb.com/MariaDB/mariadb-10.11/repo/ubuntu $(lsb_release -cs) main"
-apt install -y -o Dpkg::Options::='--force-confnew' mariadb-server-10.11 libmariadb-dev
+apt install -y -o Dpkg::Options::='--force-confnew' mariadb-server libmariadb-dev

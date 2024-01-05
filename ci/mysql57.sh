2957c64ef9146688c9274ca15524eee24998d845
Vitaliy Serov <vitaliy.serov@onlyoffice.com>
2024-01-05 13:12:58 +0200

    Fix key verification for mysql repos in ubuntu

--- ci/mysql57.sh
+++ ci/mysql57.sh
@@ -7,6 +7,7 @@ rm -fr /etc/mysql
 rm -fr /var/lib/mysql
 apt-key add support/5072E1F5.asc # old signing key
 apt-key add support/3A79BD29.asc # 5.7.37 and higher
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B7B3B788A8D3785C
 # Verify the repository as add-apt-repository does not.
 wget -q --spider http://repo.mysql.com/apt/ubuntu/dists/$(lsb_release -cs)/mysql-5.7
 add-apt-repository 'http://repo.mysql.com/apt/ubuntu mysql-5.7'

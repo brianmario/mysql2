4e18d7969abc379de89cc52618408bb38a2c1857
Satoshi MITANI <s-mitani@mercari.com>
2024-09-06 09:20:00 +0900

    Support get_server_public_key option  (#1377)
    * caching_sha2_password requests secure connection if cache is not ready on server-side.
    get_server_public_key option enables clients to create secure connection automatically even if connection is not SSL.
    
    * return error if get_server_public_key option is not supported

--- lib/mysql2/client.rb
+++ lib/mysql2/client.rb
@@ -32,11 +32,11 @@ module Mysql2
       opts[:connect_timeout] = 120 unless opts.key?(:connect_timeout)
 
       # TODO: stricter validation rather than silent massaging
-      %i[reconnect connect_timeout local_infile read_timeout write_timeout default_file default_group secure_auth init_command automatic_close enable_cleartext_plugin default_auth].each do |key|
+      %i[reconnect connect_timeout local_infile read_timeout write_timeout default_file default_group secure_auth init_command automatic_close enable_cleartext_plugin default_auth get_server_public_key].each do |key|
         next unless opts.key?(key)
 
         case key
-        when :reconnect, :local_infile, :secure_auth, :automatic_close, :enable_cleartext_plugin
+        when :reconnect, :local_infile, :secure_auth, :automatic_close, :enable_cleartext_plugin, :get_server_public_key
           send(:"#{key}=", !!opts[key]) # rubocop:disable Style/DoubleNegation
         when :connect_timeout, :read_timeout, :write_timeout
           send(:"#{key}=", Integer(opts[key])) unless opts[key].nil?

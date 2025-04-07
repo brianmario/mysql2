441b104f8f120788e86086702d96963aecec3172
Aaron Stone <aaron@serendipity.cx>
2025-04-07 00:00:49 -0700

    CI: select better MariaDB mirrors because some block GitHub Actions (#1401)

--- ci/mariadb114.sh
+++ ci/mariadb114.sh
@@ -5,6 +5,19 @@ apt purge -qq '^mysql*' '^libmysql*'
 rm -fr /etc/mysql
 rm -fr /var/lib/mysql
 
-apt-key add support/C74CD1D8.asc
-add-apt-repository "deb https://deb.mariadb.org/11.4/ubuntu $(lsb_release -cs) main"
+RELEASE=$(lsb_release -cs)
+VERSION=11.4
+
+tee <<- EOF > /etc/apt/sources.list.d/mariadb.sources
+	X-Repolib-Name: MariaDB
+	Types: deb
+	# URIs: https://deb.mariadb.org/$VERSION/ubuntu
+	URIs: https://mirror.rackspace.com/mariadb/repo/$VERSION/ubuntu
+	Suites: $RELEASE
+	Components: main main/debug
+	Signed-By: /etc/apt/keyrings/mariadb-keyring.asc
+EOF
+
+cp support/C74CD1D8.asc /etc/apt/keyrings/mariadb-keyring.asc
+apt update
 apt install -y -o Dpkg::Options::='--force-confnew' mariadb-server libmariadb-dev

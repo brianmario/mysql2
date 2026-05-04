9c505134ad70d4a5d9a438c879a540640f68d084
Yuki Fukuma <fukuma21@gmail.com>
2026-05-04 09:50:15 +0900

    CI: drop mysql55 and mysql57 setup scripts

--- ci/mysql55.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eux
-
-apt-get purge -qq '^mysql*' '^libmysql*'
-rm -fr /etc/mysql
-rm -fr /var/lib/mysql
-apt-get update -qq
-apt-get install -qq mysql-server-5.5 mysql-client-core-5.5 mysql-client-5.5 libmysqlclient-dev
